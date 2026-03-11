7ced0fe305d9d0cf925a0bfefb0a34cdff498903
AnasSfar <sfaranas13@gmail.com>
2026-03-11 12:37:53 +0100

    Update update.py

--- api/update.py
+++ api/update.py
@@ -1,20 +1,29 @@
 from http.server import BaseHTTPRequestHandler
 import json
+import subprocess
 
 
 class handler(BaseHTTPRequestHandler):
-    def do_GET(self):
-        self.send_response(200)
-        self.send_header("Content-Type", "application/json")
-        self.end_headers()
-        self.wfile.write(
-            json.dumps({"ok": True, "method": "GET"}).encode()
-        )
-
     def do_POST(self):
-        self.send_response(200)
+        try:
+            result = subprocess.run(
+                ["python", "scripts/update_streams.py"],
+                capture_output=True,
+                text=True
+            )
+
+            response = {
+                "status": "ok",
+                "stdout": result.stdout,
+                "stderr": result.stderr
+            }
+
+            self.send_response(200)
+
+        except Exception as e:
+            response = {"error": str(e)}
+            self.send_response(500)
+
         self.send_header("Content-Type", "application/json")
         self.end_headers()
-        self.wfile.write(
-            json.dumps({"ok": True, "method": "POST"}).encode()
-        )
+        self.wfile.write(json.dumps(response).encode())
